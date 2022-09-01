c2c29e64cd4267e603000c8bd3dcd199d4c0d0a4
Mr-Luckey <enginer7299@gmail.com>
2022-09-01 20:46:04 +0200

    HERE IS COMMET

---
+++ audio_waveforms-main/audio_waveforms-main/example/lib/generated_plugin_registrant.dart
@@ -0,0 +1,11 @@
+
+
+import 'package:file_picker/_internal/file_picker_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(Registrar registrar) {
+  FilePickerWeb.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
